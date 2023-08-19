93f718ce7c810862c88f795dad158c217d7481dd
unknown <qq2822856916@outlook.com>
2023-08-20 09:35:17 +1200

    Update repo test and repo implement

--- games/__init__.py
+++ games/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from .home_bp import home_bp
 from .games_bp import games_bp
+from games.adapters.memory_repository import MemoryRepository, populate
+import games.adapters.repository as repo
 
 
 def create_app():
     app = Flask(__name__)
+    repo.repo_instance = MemoryRepository()
+    populate(repo.repo_instance)
     app.register_blueprint(home_bp)
     app.register_blueprint(games_bp)
     return app
