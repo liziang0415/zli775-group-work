df57409f34b94fff58c13b31c7f01b67dc0d1b35
unknown <qq2822856916@outlook.com>
2023-08-29 13:35:47 +1200

    FINAL

--- games/gameDescription_bp.py
+++ games/gameDescription_bp.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request
-from games.services import find_game_by_title
+from games.services import find_game_by_title,get_sorted_publisher_and_genres
 
 game_description_bp = Blueprint('game_description_bp', __name__)
 
@@ -8,11 +8,12 @@ game_description_bp = Blueprint('game_description_bp', __name__)
 def game_description():
     from games.adapters.repository import repo_instance
     game_title = request.args.get('title')
+    all_publishers, all_genres = get_sorted_publisher_and_genres(repo_instance)
     if game_title:
         game = find_game_by_title(repo_instance, game_title)
         if game:
             genre_names = [genre.genre_name for genre in game.genres]
             genre_str = ', '.join(genre_names)
-            return render_template("gameDescription.html", game=game, genre_str=genre_str)
+            return render_template("gameDescription.html", game=game, genre_str=genre_str,all_genres=all_genres,all_publishers = all_publishers)
         else:
             return render_template('no_results.html'), 404
